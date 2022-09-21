231c3af289c66b40709d8c0396ddb3bb2661d0f3
nayakaw <nayakawiryatama.2016@gmail.com>
2022-09-21 11:13:55 +0700

    Create luas segitiga lingkaran else if.py

---
+++ luas segitiga lingkaran else if.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+integer = 12
+
+
+# In[2]:
+
+
+x = 12
+
+
+# In[3]:
+
+
+print(x)
+
+
+# In[4]:
+
+
+String = 'Angkatan 2022'
+
+
+# In[5]:
+
+
+Float = 3.21
+
+
+# In[7]:
+
+
+x = (4 * 8)
+
+
+# In[8]:
+
+
+x
+
+
+# In[9]:
+
+
+a = 10
+b = 5
+x = (a + b)
+
+
+# In[10]:
+
+
+x
+
+
+# In[11]:
+
+
+#Phytagoras
+
+
+# In[12]:
+
+
+# A^2 + B^2 = C^2
+# C = SQRT(A^2 + B^2)
+
+a = 4
+b = 3
+c = (a**2 + b**2)**0,5
+
+
+# In[14]:
+
+
+print(c)
+
+
+# In[20]:
+
+
+#Rumus Luas Segitiga
+#Luas = 1/2.a.t
+#Keterangan
+#a merupakan alas segitiga
+#t merupakan tinggi segitiga
+
+print ("Program Python Menghitung Luas Segitiga")
+
+a = float( input("Masukkan nilai alas : "))
+t = float( input("Masukkan nilai tinggi : "))
+
+Luas = 0.5*a*t
+
+print("Luas Segitiga = ", Luas)
+
+
+# In[19]:
+
+
+print ("Program Python Menghitung Luas Segitiga")
+
+a = float( input("Masukkan nilai alas : "))
+t = float( input("Masukkan nilai tinggi : "))
+
+Luas = 0.5*a*t
+
+print("Luas Segitiga = ", Luas)
+
+
+# In[21]:
+
+
+jari2 = 2
+rumus = 3.14 * jari2 * jari2
+
+
+# In[22]:
+
+
+satuan = input("Masukkan satuan : ")
+
+
+# In[23]:
+
+
+print("Luas dari lingkaran tersebut adalah : ", rumus, satuan)
+
+
+# In[27]:
+
+
+if(True):
+    #masuk dari sini
+    print("ini dalam if")
+    print("ini dalam if 2")
+    
+else:
+    print("ini dalam else 1")
+    
+print("ini di luar if")
+
+
+# In[ ]:
+
+
+
+
