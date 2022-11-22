12232fd945fc61f95f90b21b2ede23bbcb8ce515
nayakaw <114046129+nayakaw@users.noreply.github.com>
2022-11-22 21:39:05 +0700

    Add files via upload

---
+++ tugas 3 algoritma pemrograman Nayaka.py
@@ -0,0 +1,23 @@
+def nilai_matkul():
+    print("\nPROGRAM HASIL AKHIR STUDI ")
+    nama = input("Masukkan Nama Mahasiswa: ")
+    m1 = input("Masukkan Mata kuliah: ")
+    n1 = input("Masukkan Nilai:  ")
+    m2 = input("Masukkan Mata kuliah: ")
+    n2 = input("Masukkan Nilai:  ")
+    m3 = input("Masukkan Mata kuliah: ")
+    n3 = input("Masukkan Nilai:  ")
+    m4 = input("Masukkan Mata kuliah: ")
+    n4 = input("Masukkan Nilai:  ")
+    m5 = input("Masukkan Mata kuliah: ")
+    n5 = input("Masukkan Nilai:  ")
+        
+    print("\nHASIL NILAI AKHIR MAHASISWA \n ")
+    print("Nama Mahasiswa             : ", nama)
+    print(m1, ": ", n1)
+    print(m2, ": ", n2)
+    print(m3, ": ", n3)
+    print(m4, ": ", n4)
+    print(m5, ": ", n5)       
+        
+nilai_matkul()
